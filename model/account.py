d1a0334e6cd15102a13fd4aa8121d219a72dd99a
palaciosyost <palaciosyostin903@gmail.com>
2025-12-02 10:34:44 -0500

    update

--- model/account.py
+++ model/account.py
@@ -1,16 +1,16 @@
 from odoo import models, _
 from odoo.exceptions import UserError
 
-class AccountMove(models.Model):
-    _inherit = "account.move"
+# class AccountMove(models.Model):
+#     _inherit = "account.move"
 
-    def action_post(self):
+    # def action_post(self):
         # Primero ejecuta el posteo normal (aquí se asigna correlativo)
-        res = super().action_post()
+        # res = super().action_post()
 
         # Luego validamos con UserError
-        for record in self:
-            if record.estado_sunat not in ["01", "05"]:
-                raise UserError(_(f"La factura {record.name} no fue aceptada por la SUNAT"))
+        # for record in self:
+        #     if record.estado_sunat not in ["01", "05"]:
+        #         raise UserError(_(f"La factura {record.name} no fue aceptada por la SUNAT"))
 
-        return res
+        # return res
